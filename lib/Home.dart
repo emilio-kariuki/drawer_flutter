43ffe43031ea52413f6c9ca3984cc4349a26a1fc
emilio kariuki <70817006+emilio-kariuki@users.noreply.github.com>
2022-01-24 10:03:40 +0300

    commit

--- lib/Home.dart
+++ lib/Home.dart
@@ -75,7 +75,7 @@ class _HomeState extends State<Home> {
                       SizedBox(
                         height: 10,
                       ),
-                      Text("Emilio",
+                      Text("EcoVille",
                           style: GoogleFonts.roboto(
                             fontSize: 20,
                             fontWeight: FontWeight.bold,
